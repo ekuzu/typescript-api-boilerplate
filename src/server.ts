dd351600371b7962eec701f7f8b6030860e25fc1
ekuzu <erinckuzu@gmail.com>
2022-05-31 16:26:04 +0300

    FIX: comments removed

--- src/server.ts
+++ src/server.ts
@@ -10,16 +10,13 @@ const server = fastify({
 server.register(require('@fastify/cors'), instance => {
   return (req: FastifyRequest, callback: Function) => {
     const corsOptions = {
-      // This is NOT recommended for production as it enables reflection exploits
       origin: true
     }
 
-    // do not include CORS headers for requests from localhost
     if (/^localhost$/m.test(req.headers.origin || '')) {
       corsOptions.origin = false
     }
 
-    // callback expects two parameters: error and options
     callback(null, corsOptions)
   }
 })
